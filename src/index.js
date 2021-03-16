80111bf9bf30f7495a189e4f0b01aaab985a8064
Gage Cates <gagecates10@gmail.com>
2021-03-15 23:05:47 -0600

    fixed build error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import { handleSubmit } from './client/js/app'
-import { getCityPicture } from './client/js/getCityPic'
-import { getCoordinates } from './client/js/getCoords'
-import { getWeather } from './client/js/getWeather'
-import { postData } from './client/js/postData'
-import { updateUI } from './client/js/updateUI'
+import { handleSubmit } from './src/client/js/app'
+import { getCityPicture } from './src/client/js/getCityPic'
+import { getCoordinates } from './src/client/js/getCoords'
+import { getWeather } from './src/client/js/getWeather'
+import { postData } from './src/client/js/postData'
+import { updateUI } from './src/client/js/updateUI'
 
-import './client/style/style.scss'
-import './client/style/mobile.scss'
-import './client/pics/balloon-background.jpg'
-import './client/pics/beach-background.jpg'
+import './src/client/style/style.scss'
+import './src/client/style/mobile.scss'
+import './src/client/pics/balloon-background.jpg'
+import './src/client/pics/beach-background.jpg'
 
 
 export {
